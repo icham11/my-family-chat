6ee820e99385b295cdaa18e46756e1c5301cfa4b
Wahid Nurhisyam <ichamdaiboz@gmail.com>
2026-01-18 17:12:40 +0700

    revisi cors

--- server/index.js
+++ server/index.js
@@ -16,11 +16,25 @@ const app = express();
 const server = http.createServer(app);
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://family-chat-99.web.app",
+      "http://54.169.128.182:3000",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(passport.initialize());
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Family Chat API is running 🚀");
+});
 
 // Routes
 app.use("/api/auth", authRoutes);
